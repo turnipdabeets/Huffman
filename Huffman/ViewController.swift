cf22c5909499942efd99831e3e2d7d9910b4c58b
Anna Garcia <anna.e.garcia@aexp.com>
2018-11-21 11:14:45 -0600

    Initial Commit

---
+++ Huffman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Huffman
+//
+//  Created by Anna Elisabeth Garcia on 11/21/18.
+//  Copyright © 2018 Anna Elisabeth Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
