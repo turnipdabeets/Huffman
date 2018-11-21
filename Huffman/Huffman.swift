47c7049b46da3c85e13b7161728d290cb0f17c29
Anna Garcia <anna.e.garcia@aexp.com>
2018-11-21 11:26:33 -0600

    Set Up

---
+++ Huffman/Huffman.swift
@@ -0,0 +1,16 @@
+//
+//  Huffman.swift
+//  Huffman
+//
+//  Created by Anna Elisabeth Garcia on 11/21/18.
+//  Copyright © 2018 Anna Elisabeth Garcia. All rights reserved.
+//
+
+import Foundation
+
+class Huffman {
+    
+    init(_ input: String) {
+        <#statements#>
+    }
+}
